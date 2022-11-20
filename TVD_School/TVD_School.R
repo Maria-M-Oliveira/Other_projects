ced90ba01cc24d01393622238f226474377fe0e4
Maria <oliveira.maria.miguel@gmail.com>
2022-11-20 20:44:08 +0000

    Update TVD_School.R

--- TVD_School/TVD_School.R
+++ TVD_School/TVD_School.R
@@ -27,21 +27,21 @@ Escolas <- as.data.frame (tribble(~name, ~lon, ~lat,
                     'ESHN',-9.256104898339965,39.08983507024364,
                     'Maxial',-9.17126663153978 ,39.14211512622054,
                     'ESHN',-9.256104898339965,39.08983507024364,
-                    'Outeiro',-9.180702720365444 ,39.19046046412656,
+                    'Outeiro da Cabeça',-9.180702720365444 ,39.19046046412656,
                     'ESHN',-9.256104898339965,39.08983507024364,
-                    'Monte_Redondo',-9.200147572174581 ,39.11771656538302,
+                    'Monte Redondo',-9.200147572174581 ,39.11771656538302,
                     'ESHN',-9.256104898339965,39.08983507024364,
                     'Ereira',-9.154956053996555, 39.11972513696694,
                     'ESHN',-9.256104898339965,39.08983507024364,
-                    'Mataces',-9.212182112082875 , 39.092667568333056,
+                    'Matacães',-9.212182112082875 , 39.092667568333056,
                     'ESHN',-9.256104898339965,39.08983507024364,
                     'Ramalhal',-9.233219816635028, 39.14665975564143,
                     'ESHN',-9.256104898339965,39.08983507024364,
-                    'Aldeia_grande',-9.15510577487542,39.153848821095515,
+                    'Aldeia Grande',-9.15510577487542,39.153848821095515,
                     'ESHN',-9.256104898339965,39.08983507024364,
                     'Ameal', -9.23636962430571, 39.1345848415169,
                     'ESHN',-9.256104898339965,39.08983507024364,
-                    'Abrunehira',-9.207221648365467, 39.13779854872996
+                    'Abrunheira',-9.207221648365467, 39.13779854872996
                     ))
 Escolas1 <- Escolas
  #Converting schools df so it is usable for mapview  
@@ -52,10 +52,26 @@ Escolas2 <- select(Escolas, lon, lat)
 Rotas<- ors_directions(Escolas2, output = "sf") 
 #output sf so i can use it in mapview to combine iso and route on the same map
 
-pal<- c("deepskyblue", "deepskyblue1", "deepskyblue2","deepskyblue3","dodgerblue","dodgerblue1","dodgerblue2","dodgerblue3","cadetblue1","cadetblue2")
+pal<- c("deepskyblue", 
+        "deepskyblue1", 
+        "deepskyblue2",
+        "deepskyblue3",
+        "dodgerblue",
+        "dodgerblue1",
+        "dodgerblue2",
+        "dodgerblue3",
+        "cadetblue1",
+        "cadetblue2")
 ESHN_iso<- mapview(ranges, alpha.regions = 0.2, homebutton = FALSE, legend = FALSE, col.regions = ("cornflowerblue"))
 Map_Rotas <- mapview(Rotas)
-Map_Pontos <- mapview(Escolas1, col.regions=pal)
+Map_Pontos <- mapview(Escolas1, legend= FALSE)
 
 Mapa_final <- ESHN_iso + Map_Rotas + Map_Pontos
-Mapa_final
+Mapa_final@map %>%
+  addStaticLabels(Escolas1, Escolas1$name,
+                  noHide = TRUE,
+                  direction = 'left',
+                  textOnly = TRUE,
+                  textsize = "15px",
+                  style= list("font-weight"="bold"))
+
