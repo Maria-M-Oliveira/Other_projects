d70dd7a5e1bbc1f579e5157306307f48b47c9c8a
Maria <oliveira.maria.miguel@gmail.com>
2022-11-20 20:02:31 +0000

    Update TVD_School.R

--- TVD_School/TVD_School.R
+++ TVD_School/TVD_School.R
@@ -20,29 +20,48 @@ ranges <- ranges[rev(values)]
 
 names(ranges) <- sprintf("%s min", as.numeric(names(ranges))/60)
 
-ESHN_iso<- mapview(ranges, alpha.regions = 0.2, homebutton = FALSE, legend = FALSE)
-ESHN_iso
+
 
 # adding other relevant schools
-Maxial <- data.frame(lon = c(-9.17126663153978) , lat = c(39.14211512622054))
-Outeiro <- data.frame(lon = c(-9.180702720365444) , lat = c(39.19046046412656))
-Monte_redondo <- data.frame(lon = c(-9.200147572174581) , lat = c(39.11771656538302))
-Ereira <- data.frame(lon = c(-9.154956053996555) , lat = c(39.11972513696694))
-Matacaes <- data.frame(lon = c(-9.212182112082875) , lat = c(39.092667568333056))
-Ramalhal <- data.frame(lon = c(-9.233219816635028) , lat = c(39.14665975564143)) 
-Aldeia_grande <-data.frame(lon = c(-9.15510577487542) , lat = c(39.153848821095515)) 
-Ameal <- data.frame(lon = c(-9.23636962430571) , lat = c(39.1345848415169)) 
-Abrunheira <- data.frame(lon = c(-9.207221648365467) , lat = c(39.13779854872996)) 
 
 # Creating df for routing
-schools <- rbind(ESHN,Maxial,ESHN, Outeiro, ESHN, Monte_redondo, ESHN, Ereira, ESHN, Matacaes,ESHN,Ramalhal, ESHN,Aldeia_grande,ESHN,Ameal,ESHN,Abrunheira)
-
+Escolas <- as.data.frame (tribble(~name, ~lon, ~lat,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Maxial',-9.17126663153978 ,39.14211512622054,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Outeiro',-9.180702720365444 ,39.19046046412656,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Monte_Redondo',-9.200147572174581 ,39.11771656538302,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Ereira',-9.154956053996555, 39.11972513696694,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Mataces',-9.212182112082875 , 39.092667568333056,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Ramalhal',-9.233219816635028, 39.14665975564143,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Aldeia_grande',-9.15510577487542,39.153848821095515,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Ameal', -9.23636962430571, 39.1345848415169,
+                    'ESHN',-9.256104898339965,39.08983507024364,
+                    'Abrunehira',-9.207221648365467, 39.13779854872996
+                    ))
+Escolas1 <- Escolas
  #Converting schools df so it is usable for mapview  
-coordinates(schools) <- c("lon", "lat")
-proj4string(schools) <- CRS("+init=epsg:4326")
+coordinates(Escolas1) <- c("lon", "lat")
+proj4string(Escolas1) <- CRS("+init=epsg:4326")
 
-test<- ors_directions(schools, output = "sf") #output sf so i can use it in mapview to combine iso and route on the same map
+Escolas2 <- select(Escolas, lon, lat)
+Rotas<- ors_directions(Escolas2, output = "sf") 
+#output sf so i can use it in mapview to combine iso and route on the same map
 
-mapview(test) + ESHN_iso + mapview(schools)
+ESHN_iso<- mapview(ranges, alpha.regions = 0.2, homebutton = FALSE, legend = FALSE, col.regions = ("cornflowerblue"))
+Map_Rotas <- mapview(Rotas)
+Map_Pontos <- mapview(Escolas1, col.regions=list("cyan","blue")) %>%
+  addStaticLabels(label = Escolas1$name,
+                  noHide = TRUE,
+                  direction = 'top',
+                  textOnly = TRUE,
+                  textsize = "20px")
 
-
+Mapa_final <- ESHN_iso + Map_Rotas + Map_Pontos
+Mapa_final
